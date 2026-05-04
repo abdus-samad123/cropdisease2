bd39667a8d56b856d6080215ac206998865d468b
Abdus samad <125046921+abdus-samad123@users.noreply.github.com>
2026-05-04 23:15:56 +0530

    Add image prediction function to detect disease

---
+++ image_model.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import numpy as np
+
+def predict_image(image):
+    img = np.array(image)
+    avg = img.mean()
+
+    if avg < 100:
+        return "Disease Detected"
+    else:
+        return "Healthy"
